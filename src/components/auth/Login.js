02fd7d04321f26fe59de79e82205d26a3d6e28cc
Tami Pipoca <tamisakita@hotmail.com>
2020-12-09 22:19:47 -0300

    config app.js and index.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,3 +1,13 @@
 import React from 'react';
-import axios from 'axios';
-import * as yup from 'yup';
+// import axios from 'axios';
+// import * as yup from 'yup';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>LOGINNN</h1>
+    </div>
+  )
+}
+
+export default Login;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,2 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+const navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>CONDOBOOK</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default navbar;
