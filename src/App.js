9eb4fb4170c10598dfce74077a95338ea6e7bb22
Tami Pipoca <tamisakita@hotmail.com>
2020-12-10 22:32:38 -0300

    started login, addResidents and Navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
 import Login from './components/auth/Login';
+import AddResidents from './components/residents/AddResidents';
 
 
 class App extends Component {
+  state = {
+    isUserAuthenticated: false,
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar isUserAuth={this.state.isUserAuthenticated} role="sindico"/>
 
         <Switch>
+          {/* rotas publicas */}
           <Route exact path="/login" component={Login} />
+          {/* rotas privadas */}
+          <Route exact path="/residents/register" component={AddResidents} />
         </Switch>
       </div>
     );
